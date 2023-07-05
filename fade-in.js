dd8eef4060168f367ca0925365e60262584ea4de
travis-the-coder <55335455+travis-the-coder@users.noreply.github.com>
2023-07-05 18:04:56 -0400

    1. added the following classes [col fade-in, slide -in from-left,] 2. added the corresponding fade-in classes in CSS as well as their appear classes 3. adjusted fade-in -left animation 4. created .js file and added fade-in and slide-in code in javascript

---
+++ fade-in.js
@@ -0,0 +1,32 @@
+  const faders = document.querySelectorAll('.fade-in');
+  const appearOptions = {
+    threshold: 0,
+    rootMargin: "0px, 0px, -100px, 0px"
+  };
+    const sliders = document.querySelectorAll(".slide-in");
+
+
+
+  const appearOnScroll = new IntersectionObserver
+  (function(entries, appearOnScroll) {
+
+    entries.forEach(entry => {
+        if(!entry.isIntersecting) {
+            return;}
+            else {
+                entry.target.classList.add('appear');
+                appearOnScroll.unobserve(entry.target);
+            }
+    })
+
+  },appearOptions);
+
+faders.forEach(fader => {
+    appearOnScroll.observe(fader);
+
+});
+
+sliders.forEach(slider => {
+    appearOnScroll.observe(slider);
+
+});
